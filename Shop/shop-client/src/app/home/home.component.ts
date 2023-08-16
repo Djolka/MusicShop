16064b47542421919d50ac0934b3cfa874881113
Djordje <djordjem00@gmail.com>
2023-08-16 18:31:59 +0200

    Add filtering by name, price, and brand name

--- Shop/shop-client/src/app/home/home.component.ts
+++ Shop/shop-client/src/app/home/home.component.ts
@@ -1,44 +1,74 @@
 import { Component } from '@angular/core';
 import { ProductService } from '../services/product.service';
 import { Product } from '../models/product.model';
-import { Observable } from 'rxjs';
+import { Observable, map } from 'rxjs';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+	selector: 'app-home',
+	templateUrl: './home.component.html',
+	styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
 	public products: Observable<Product[]>
 
-  	constructor (private productService: ProductService) {
+	constructor(private productService: ProductService) {
 		this.products = this.productService.getProducts()
 	}
 
 	public onChange(event: any) {
-		switch(event.target.value) {
-			case 'priceLH':{
-				console.log('priceLH')
+		switch (event.target.value) {
+			case 'priceLH': {
+				this.products = this.products.pipe(map((data) => {
+					data.sort((a, b) => {
+						return a.price < b.price ? -1 : 1;
+					});
+					return data;
+				}))
 				break
 			}
-			case 'priceHL':{
-				console.log('priceHL')
+			case 'priceHL': {
+				this.products = this.products.pipe(map((data) => {
+					data.sort((a, b) => {
+						return a.price > b.price ? -1 : 1;
+					});
+					return data;
+				}))
 				break
 			}
-			case 'nameAZ':{
-				console.log('nameAZ')
+			case 'nameAZ': {
+				this.products = this.products.pipe(map((data) => {
+					data.sort((a, b) => {
+						return a.name.toLocaleLowerCase() < b.name.toLocaleLowerCase() ? -1 : 1;
+					});
+					return data;
+				}))
 				break
 			}
-			case 'nameZA':{
-				console.log('nameZA')
+			case 'nameZA': {
+				this.products = this.products.pipe(map((data) => {
+					data.sort((a, b) => {
+						return a.name.toLocaleLowerCase() > b.name.toLocaleLowerCase() ? -1 : 1;
+					});
+					return data;
+				}))
 				break
 			}
-			case 'brandAZ':{
-				console.log('brandAZ')
+			case 'brandAZ': {
+				this.products = this.products.pipe(map((data) => {
+					data.sort((a, b) => {
+						return a.manufacturer.toLocaleLowerCase() < b.manufacturer.toLocaleLowerCase() ? -1 : 1;
+					});
+					return data;
+				}))
 				break
 			}
-			case 'brandZA':{
-				console.log('brandZA')
+			case 'brandZA': {
+				this.products = this.products.pipe(map((data) => {
+					data.sort((a, b) => {
+						return a.manufacturer.toLocaleLowerCase() > b.manufacturer.toLocaleLowerCase() ? -1 : 1;
+					});
+					return data;
+				}))
 				break
 			}
 		}
